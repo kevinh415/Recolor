8e516b3c93f4e6f202def6530212d9034e126c47
Kevin Hong <kevinh415@gmail.com>
2019-01-02 21:48:51 -0500

    Removed use of Tkinter, now outputs randomly colorized image

--- popup.py
+++ popup.py
@@ -10,17 +10,19 @@ import cv2
 from matplotlib import pyplot as plt
 import random
 from colormath.color_objects import LabColor
-from colormath.color_diff import delta_e_cie1976
+from colormath.color_diff import delta_e_cie2000
 
 def main(filename,outputname):
     # get the name of the image and save the width and height as x and y
-    i = PIL.Image.open(filename).convert('RGB')
-    (x,y) = i.size
-
+    im = PIL.Image.open(filename).convert('RGB')
+    (x,y) = im.size
+    print("Image size:")
+    print(x,y)
     rgb = io.imread(filename)
     # get the lab color space for comparing similar colors later on
     lab = color.rgb2lab(rgb)
 
+    '''
     root = Tk()
     #setting up a tkinter canvas with scrollbars
     frame = Frame(root, bd=2, relief=SUNKEN)
@@ -38,32 +40,15 @@ def main(filename,outputname):
     frame.pack(fill=BOTH,expand=1)
 
     try:
-        img = ImageTk.PhotoImage(i)
+        img = ImageTk.PhotoImage(im)
     except:
         print("Error with ImageTk.PhotoImage")
 
     canvas.create_image(0,0,image=img,anchor="nw")
     canvas.config(scrollregion=canvas.bbox(ALL))
-
-    # function to be called when mouse is clicked
-    def printcoords(event):
-        # outputting x and y coords to console
-        print (event.x,event.y)
-        # call the popup method with the x and y coordinates of the clicked location
-        popup(event.x, event.y, lab, i, outputname)
-
-    # mouseclick event
-    canvas.bind("<Button 1>",printcoords)
-
-    root.mainloop()
-
-def popup(x, y, lab, image, outputname):
-
-    (l,a,b) = lab[y,x]
-    original = LabColor(lab_l=l, lab_a = a, lab_b = b)
-    ### get the gradient magnitudes
-    arr = np.array(image.convert('L'))
-    pix = image.load()
+    '''
+    # get gradient magnitudes
+    arr = np.array(im.convert('L'))
 
     # x gradient
     image_x = np.zeros(arr.shape)
@@ -77,32 +62,64 @@ def popup(x, y, lab, image, outputname):
     gradient = np.sqrt(image_x**2 + image_y**2)
 
 
+
+    # create an array with all 0's to keep track of which pixels have changed color and which pixels have not
+    checker = np.zeros((len(gradient[0]),len(gradient)))
+    # add gradient lines
+    gradient_lines(im, gradient, checker)
+
+    # loop through each pixel to see if the color has already been changed
+    # if the color has not been changed, then call the method to change all
+    # other pixels with similar colors
+    for i in range(0, len(checker)): #width
+        for j in range(0,len(checker[0])): #height
+            if checker[i,j] == 0:
+                change_color(i,j, lab, im, gradient, checker)
+
+    # function to be called when mouse is clicked
+    def printcoords(event):
+        # outputting x and y coords to console
+        print (event.x,event.y)
+        # call the popup method with the x and y coordinates of the clicked location
+        popup(event.x, event.y, lab, i, gradient, checker)
+
+    # mouseclick event
+    #canvas.bind("<Button 1>",printcoords)
+
+    ###root.mainloop()
+
+    im.show()
+    im.save(outputname)
+    print("DONE")
+
+def change_color(x, y, lab, image, gradient, checker):
+    (l,a,b) = lab[y,x] #height,width
+    original = LabColor(lab_l=l, lab_a = a, lab_b = b)
+    ### get the gradient magnitudes
+    pix = image.load()
+
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    print(r,g,b)
-    for i in range(0, len(arr[0])):
-        for j in range(0,len(arr)):
+
+    for i in range(0, len(gradient[0])): #width
+        for j in range(0,len(gradient)): #height
             l2,a2,b2 = lab[j,i]
             current = LabColor(lab_l=l2, lab_a = a2, lab_b = b2)
-            #print(pix[i,j])
-            #pix[i,j] = gradient[i,j]
-            #print(type(gradient[i,j]))
-            #color_difference = np.sqrt((l-l2)**2 + (a-a2)**2 + (b-b2)**2)
-            delta_e = delta_e_cie1976(original, current)
 
-            #print(color_difference)
-            if delta_e < 50:
-                pix[i,j] = (r,g,b)
+            if checker[i,j] == 0:
+                delta_e = delta_e_cie2000(original, current,Kl=1,Kc=1,Kh=1)
+                if delta_e < 20:
+                    pix[i,j] = (r,g,b)
+                    checker[i,j] = 1
 
-            elif gradient[j,i] > 70:
-                pix[i,j] = 255
-
-            else:
+def gradient_lines(image, gradient,checker):
+    pix = image.load()
+    for i in range(0, len(gradient[0])): #width
+        for j in range(0,len(gradient)): #height
+            if gradient[j,i] > 60:
                 pix[i,j] = 0
-
-    # display the final image
-    image.show()
+                checker[i,j] = 1
 
 if __name__ == "__main__":
     filename = sys.argv[1]
